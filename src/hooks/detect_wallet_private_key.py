9d843e3c713d1e1ed76dab3430c08eef30a7fa81
vic-en <victoreni14@gmail.com>
2022-02-01 10:49:36 +0100

    update

--- src/hooks/detect_wallet_private_key.py
+++ src/hooks/detect_wallet_private_key.py
@@ -16,17 +16,20 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
         with open(filename, 'r') as f:
             lines = f.readlines()
             for line in lines:
-                if re.search("[0-9A-Fa-f]{64}", line):
-                    for allowed in WHITELIST:
-                        if not f"noqa: {allowed}" in line:
-                            private_key_files.append(filename)
-                            break
+                if filename in private_key_files:
+                    break
+                elif re.search("[0-9A-Fa-f]{64}", line):
+                    whitelisted = [f"noqa: {allowed}" in line for allowed in WHITELIST]
+                    if not any(whitelisted):
+                        private_key_files.append(filename)
+                        break
+                    
 
     if private_key_files:
         for private_key_file in private_key_files:
             print(f'{private_key_file} contains line(s) that include same pattern as a wallet private key.')
-        print('Either remove those lines or add a "noqa: {REASON}" tag comment on such lines.')
-        print(f'REASON can be on of the following: {WHITELIST}.')
+        print('Either remove those line(s) or add a "noqa: {REASON}" tag on such lines.')
+        print(f'REASON can be one of the following: {WHITELIST}.')
         return 1
     else:
         return 0
