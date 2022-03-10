78f0683233a09c68a072fd52740d32c0376d4f0f
Martin Kou <martin@hummingbot.io>
2022-03-10 01:12:38 -0800

    Fixed detect private key error when committing binary files.

--- src/hooks/detect_wallet_private_key.py
+++ src/hooks/detect_wallet_private_key.py
@@ -9,10 +9,14 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*', help='Filenames to check')
     args = parser.parse_args(argv)
+    filename_pattern = re.compile(r"\.(py|pyx|ts|js)$", re.I)
 
     private_key_files = []
 
     for filename in args.filenames:
+        # Only scan Python, Cython, Typescript and Javascript files.
+        if filename_pattern.search(filename) is None:
+            continue
         with open(filename, 'r') as f:
             lines = f.readlines()
             for line in lines:
